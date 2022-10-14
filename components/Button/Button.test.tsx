0d9bfc0d37bcc3b73f863f612809b0306048f66e
Carina <revuwem@gmail.com>
2022-10-14 22:15:21 +0400

    test(Button): describe Button test cases

---
+++ components/Button/Button.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Button from "./Button";
+
+describe("Button", () => {
+  it("should render children props", () => {
+    render(<Button onClick={() => {}}>Click me</Button>);
+    const button = screen.getByText(/Click me/i);
+    expect(button).toBeInTheDocument();
+  });
+
+  it("should fire onClick handler", () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick}>Click me</Button>);
+    const button = screen.getByText(/Click me/i);
+    fireEvent.click(button);
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+});
