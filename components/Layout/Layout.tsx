e6c57397ffe7c76ef766c11c9b4f67190c70e842
Carina <revuwem@gmail.com>
2022-10-14 20:22:38 +0400

    feat: create Layout

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Footer from "../Footer";
+import Header from "../Header";
+import styles from "./Layout.module.css";
+
+interface ILayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<ILayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Droppe Xmas</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className={styles.layout}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Layout;
