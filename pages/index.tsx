64482b9a88476bfe1bf23207fcb99b1315c21106
Carina <revuwem@gmail.com>
2022-10-15 01:55:52 +0400

    feat(Home): finish layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import Wishlist from "../components/Wishlist";
+import Button from "../components/Button";
+import FilterPriceForm from "../components/FilterPriceForm";
 import { Wishlist as WishlistT } from "../types/Wishlist";
 import { getAllWishlists } from "../services/api";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const [wishlistList, setWishlistList] = useState<WishlistT[]>([]);
@@ -12,30 +15,29 @@ const Home: NextPage = () => {
 
   const onApproveWishlist = () => console.log("approved all");
   const onDiscardWishlist = () => console.log("discarded all");
+  const onFilterPriceFormSubmit = () => console.log("submit filter price");
 
   return (
     <>
-      <h2>Manage Wishlists</h2>
-      <div className="controls">
-        <div>
-          <span>Filter by price, €</span>
-          <input type="text" id="priceMin" placeholder="0" />
-          <input type="text" id="priceMax" placeholder="250" />
-          <button>Apply</button>
-        </div>
-        <button>Approve all Wishlists</button>
-      </div>
-      <div className="page-content">
-        <div>
-          {wishlistList.map((item) => (
-            <Wishlist
-              key={item.id}
-              id={item.id}
-              products={item.products}
-              onApproveAll={onApproveWishlist}
-              onDiscardAll={onDiscardWishlist}
-            />
-          ))}
+      <h2 className={styles.heading}>Manage Wishlists</h2>
+
+      <div className="pageContainer">
+        <div className="content">
+          <div className={styles.controls}>
+            <FilterPriceForm onSubmit={onFilterPriceFormSubmit} />
+            <Button onClick={() => {}}>Approve all Wishlists</Button>
+          </div>
+          <div className="list">
+            {wishlistList.map((item) => (
+              <Wishlist
+                key={item.id}
+                id={item.id}
+                products={item.products}
+                onApproveAll={onApproveWishlist}
+                onDiscardAll={onDiscardWishlist}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </>
