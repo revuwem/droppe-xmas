34c9b9f35d6040a411f306b4d671f026952b5b3b
Carina <revuwem@gmail.com>
2022-10-16 01:34:51 +0400

    refactor(types): change WishlistProduct to Product, add types for Product fields

--- types/Wishlist.tsx
+++ types/Wishlist.tsx
@@ -1,7 +1,25 @@
-export type WishlistProduct = { productId: number; quantity: number };
+type ProductDetails = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
+
+export type Product = {
+  productId: number;
+  quantity: number;
+  details: ProductDetails;
+  isApproved: boolean | undefined;
+};
 
 export type Wishlist = {
   id: number;
   userId: number;
-  products: WishlistProduct[];
+  products: Product[];
 };
