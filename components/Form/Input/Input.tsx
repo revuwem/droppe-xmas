58a1a717e5884ddcc0bb25a9602bcb87643a5a19
Carina <revuwem@gmail.com>
2022-10-16 02:56:40 +0400

    refactor(Input): add value props

--- components/Form/Input/Input.tsx
+++ components/Form/Input/Input.tsx
@@ -2,12 +2,19 @@ import styles from "./Input.module.css";
 
 interface IInputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const Input: React.FC<IInputProps> = ({ id, type, placeholder, onChange }) => {
+const Input: React.FC<IInputProps> = ({
+  id,
+  type,
+  placeholder,
+  value,
+  onChange,
+}) => {
   return (
     <input
       type={type}
       id={id}
       placeholder={placeholder}
+      value={value}
       onChange={onChange}
       className={styles.input}
     />
